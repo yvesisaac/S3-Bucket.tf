ce41baaeaeb9ad17d039a62828d867037a1a1d26
Isaac Yves <93200326+yvesisaac@users.noreply.github.com>
2023-11-27 14:32:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
 
+terraform {
+  required_version = "1.6.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "projeto-bucket" {
+  bucket = "projeto-bucket-200326"
+  acl    = "private"
+
+  tags = {
+    Name        = "bucket-projeto"
+    Environment = "Dev"
+    Managedby   = "Terraform"
+  }
+}
